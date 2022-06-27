71515dea28d2348c711f9fad8f7a96d057c24f6e
Brandon Thomas <hazeion@gmail.com>
2022-06-26 17:50:37 -0700

    eslint

--- functions/take-screenshot.js
+++ functions/take-screenshot.js
@@ -1,71 +1,65 @@
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
-
-
-
+const chromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
 exports.handler = async (event, context) => {
-    try {
+  try {
+    const params = JSON.parse(event.body)
+    const pageToScreenshot = params.pageToScreenshot
 
-        const params = JSON.parse(event.body);
-        const pageToScreenshot = params.pageToScreenshot;
+    const browser = await puppeteer.launch({
+      /* use this when deploying */
+    //   executablePath: await chromium.executablePath,
+    //   args: chromium.args,
+    //   defaultViewport: chromium.defaultViewport,
+    //   headless: chromium.headless,
 
-        const browser = await puppeteer.launch({
+      /* use these when on dev */
+      executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+      args: [],
+      headless: true
+    })
 
-        /* use this when deploying */
-        executablePath: await chromium.executablePath,
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        headless: chromium.headless,
+    const page = await browser.newPage()
 
-        /* use these when on dev */
-        // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
-        // args: [],
-        // headless: true,
-        });
-        
-        const page = await browser.newPage();
-        
-        await page.setViewport({
-            width: 800,
-            height: 600,
-            deviceScaleFactor: .75,
-            });
+    await page.setViewport({
+      width: 800,
+      height: 600,
+      deviceScaleFactor: 0.75
+    })
 
-        await page.goto(pageToScreenshot,  { waitUntil: 'load' });
+    await page.goto(pageToScreenshot, { waitUntil: 'load' })
 
-        const title = await page.title();
+    const title = await page.title()
 
-        let description = "";
+    let description = ''
 
-        const url = page.url();
+    const url = page.url()
 
-        const screenshot = await page.screenshot({encoding: 'binary'});
+    const screenshot = await page.screenshot({ encoding: 'binary' })
 
-        if(await page.$('meta[property="og:description"]')) {
-           description = await page.$eval('meta[property="og:description"]', element => element.content);
-        } 
+    if (await page.$('meta[property="og:description"]')) {
+      description = await page.$eval('meta[property="og:description"]', (element) => element.content)
+    }
 
-        await browser.close();
-        
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                status: 'OK',
-                message: `Completed screenshot of ${title}`,
-                page: {
-                    title,
-                    description,
-                    url
-                },
-                buffer: screenshot
+    await browser.close()
 
-            })
-        }
-        browser.disconnect();
-    } catch(error) {
-        console.log(error)
-        document.getElementById('result').textContent = `Error: ${error.toString()}`;
-    } 
-    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        status: 'OK',
+        message: `Completed screenshot of ${title}`,
+        page: {
+          title,
+          description,
+          url
+        },
+        buffer: screenshot
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    document.getElementById(
+      'result'
+    ).textContent = `Error: ${error.toString()}`
+  }
 }
--- public/script.js
+++ public/script.js
@@ -1,76 +1,60 @@
+// Creates jpeg image from ArrayBuffer data - found this
+function bufferToImageUrl (buffer) {
+  const arrayBufferView = new Uint8Array(buffer)
+  const blob = new Blob([arrayBufferView], { type: 'image/jpeg' })
+  const urlCreator = window.URL || window.webkitURL
+  const imageURL = urlCreator.createObjectURL(blob)
 
-// Creates jpeg image from ArrayBuffer data - found this 
-function bufferToImageUrl(buffer) {
-
-    const arrayBufferView = new Uint8Array(buffer);
-    const blob = new Blob([arrayBufferView], { type: "image/jpeg" });
-    const urlCreator = window.URL || window.webkitURL;
-    const imageURL = urlCreator.createObjectURL(blob);
-
-    return imageURL;
+  return imageURL
 }
 
 // Takes value in form and assigns it to pageToScreenshot for the fetch POST
-async function clickPost() {
-    try {
+async function clickPost () {
+  try {
+    const pageToScreenshot = document.getElementById('page').value
 
-        const pageToScreenshot = document.getElementById('page').value;
+    if (!pageToScreenshot.includes('https://') && !pageToScreenshot.includes('http://')) {
+      document.getElementById('result').textContent =
+        'Not valid website.. did you include http(s):// ?'
+    } else {
+      const titleDiv = document.getElementById('title')
+      const descripDiv = document.getElementById('description')
 
-        if (!pageToScreenshot.includes("https://") && !pageToScreenshot.includes("http://")) {
-            document.getElementById('result').textContent = "Not valid website.. did you include http(s):// ?";   
-        } else {
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json; charset=utf-8' },
+        body: JSON.stringify({ pageToScreenshot })
+      }
 
-        let titleDiv = document.getElementById('title');
-        let descripDiv = document.getElementById('description');
+      document.getElementById('result').textContent = 'Please Wait...'
 
-        const options = {
-            method: "POST",
-            headers: { "Content-Type": "application/json; charset=utf-8" },
-            body: JSON.stringify({ pageToScreenshot: pageToScreenshot })
+      // fetch meta information in the form of json
+      const response = await fetch('.netlify/functions/take-screenshot', options)
+      const data = await response.json()
 
-        }
+      // assigns the data from the json to title and page description
+      const titleName = await data.page.title
+      const urlName = await data.page.url
+      const pageDescription = await data.page.description
 
+      // creates image element and convert / injects buffer to image src.
+      const img = document.createElement('img')
+      img.src = bufferToImageUrl(data.buffer.data)
 
-        document.getElementById('result').textContent = "Please Wait...";
-
-        // fetch meta information in the form of json
-        const response = await fetch('.netlify/functions/take-screenshot', options);
-        const data = await response.json();
-
-        // assigns the data from the json to title and page description
-  
-        const titleName =  await data.page.title; 
-        
-        const urlName = await data.page.url;
-        
-        const pageDescription = await data.page.description;
-
-
-
-        // creates image element and convert / injects buffer to image src.
-        const img = document.createElement('img');
-        img.src = bufferToImageUrl(data.buffer.data);
-
-
-        // assigns title, image, and description information
-        titleDiv.innerHTML = titleName;
-        document.getElementById('result').innerHTML = img.outerHTML;
-        descripDiv.innerHTML = pageDescription;
-
+      // assigns title, image, and description information
+      titleDiv.innerHTML = titleName
+      document.getElementById('result').innerHTML = img.outerHTML
+      descripDiv.innerHTML = pageDescription
     }
-        
-    } catch(error){
-        console.log(error)
-        document.getElementById('result').textContent = `Error: ${error.toString()}`;
-    }
+  } catch (error) {
+    console.log(error)
+    document.getElementById('result').textContent = `Error: ${error.toString()}`
+  }
 }
 
-/* */
-
-document.getElementById('submit').addEventListener("click", (event) => { 
-    event.preventDefault();
-    document.getElementById('title').innerHTML = "";
-    document.getElementById('description').innerHTML = "";
-    clickPost();
-});
-// document.getElementById('submit').addEventListener("click", clickPost);
+document.getElementById('submit').addEventListener('click', (event) => {
+  event.preventDefault()
+  document.getElementById('title').innerHTML = ''
+  document.getElementById('description').innerHTML = ''
+  clickPost()
+})
